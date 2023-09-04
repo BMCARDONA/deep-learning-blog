1cc87dc00ba7e5503d6f2299187074ef6d66e248
Bradley <bradleymathematics@gmail.com>
2023-09-04 16:15:12 -0400

    add post

---
+++ posts/pytorch-model/cnn_model.py
@@ -0,0 +1,81 @@
+# class Mnist_CNN(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+#         self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
+#         self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+
+#     def forward(self, xb):
+#         xb = xb.view(-1, 1, 28, 28)
+#         xb = F.relu(self.conv1(xb))
+#         xb = F.relu(self.conv2(xb))
+#         xb = F.relu(self.conv3(xb))
+#         xb = F.avg_pool2d(xb, 4)
+#         return xb.view(-1, xb.size(1))
+
+# lr = 0.1
+
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x):
+        return self.func(x)
+
+
+def preprocess(x):
+    return x.view(-1, 1, 28, 28)
+
+model = nn.Sequential(
+    Lambda(preprocess),
+    nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.AvgPool2d(4),
+    Lambda(lambda x: x.view(x.size(0), -1)),
+)
+
+opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+fit(epochs, model, loss_func, opt, train_dl, valid_dl)
+
+
+def preprocess(x, y):
+    return x.view(-1, 1, 28, 28), y
+
+
+class WrappedDataLoader:
+    def __init__(self, dl, func):
+        self.dl = dl
+        self.func = func
+
+    def __len__(self):
+        return len(self.dl)
+
+    def __iter__(self):
+        for b in self.dl:
+            yield (self.func(*b))
+
+train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
+train_dl = WrappedDataLoader(train_dl, preprocess)
+valid_dl = WrappedDataLoader(valid_dl, preprocess)
+
+
+model = nn.Sequential(
+    nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.AdaptiveAvgPool2d(1),
+    Lambda(lambda x: x.view(x.size(0), -1)),
+)
+
+opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+fit(epochs, model, loss_func, opt, train_dl, valid_dl)
